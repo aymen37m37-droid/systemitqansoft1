b063d5f551e101bac86837fb4546db362ea0f94a
aymen37m37-droid <aymen37m37@gmail.com>
2026-07-24 08:19:50 +0300

    Delete electron-app directory

--- electron-app/src/preload/index.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  getAppVersion: () => ipcRenderer.invoke("app-version"),
-  getApiPort: () => ipcRenderer.invoke("get-port"),
-});
